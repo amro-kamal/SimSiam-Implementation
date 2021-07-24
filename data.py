ce5d5c1ff4abd23876ba9e863f9835e8ddde7197
Amro Abbas <37993690+amro-kamal@users.noreply.github.com>
2021-07-24 22:58:36 +0000

    first upload

---
+++ data.py
@@ -0,0 +1,68 @@
+from torchvision.transforms import transforms
+import torchvision
+
+def simsiam_cifar10_loader(batch_size):
+  
+    train_data = torchvision.datasets.CIFAR10('data/train',train=True,download=True, transform=Transform())
+    val_data = torchvision.datasets.CIFAR10('data/val',train=False,download=True, transform=Transform())
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size,num_workers=4)
+
+    return train_loader, val_loader
+
+def cifar10_loader(batch_size):
+    train_transforms = transforms.Compose([
+                                        transforms.RandomResizedCrop(32) ,
+                                        transforms.ToTensor()  ,
+                                        transforms.Normalize( mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+    ])
+
+    val_transforms = transforms.Compose([
+                                        transforms.Resize(32) ,
+                                        transforms.ToTensor()  ,
+                                        transforms.Normalize( mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+    ])
+    train_data = torchvision.datasets.CIFAR10('data/train',train=True,download=True, transform=train_transforms)
+    val_data = torchvision.datasets.CIFAR10('data/val',train=False,download=True, transform=val_transforms)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size,num_workers=4)
+
+    return train_loader, val_loader
+
+class Transform:
+    def __init__(self):
+        self.transform = transforms.Compose([
+            transforms.RandomResizedCrop(32, scale=(0.2,1.0)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                        saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                    mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+        ])
+        self.transform_prime = transforms.Compose([
+            transforms.RandomResizedCrop(32, scale=(0.2,1.0)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                        saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                    mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+        ])
+
+    def __call__(self, x):
+        y1 = self.transform(x)
+        y2 = self.transform_prime(x)
+        return y1, y2
+
+
---
+++ main.py
@@ -0,0 +1,116 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+import torchvision
+import torch.nn.functional as F
+from pytorch_lightning import seed_everything, LightningModule, Trainer
+from pytorch_lightning.callbacks import EarlyStopping
+from torch import nn, optim, rand, sum as tsum, reshape, save
+from torch.utils.data import DataLoader, Dataset
+from pytorch_lightning.callbacks import LearningRateMonitor
+import pl_bolts
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+import argparse  
+from resnet import resnet18 
+import os
+from knn_predictor import BenchmarkModule
+from data import simsiam_cifar10_loader, cifar10_loader
+
+
+
+parser.add_argument('--epochs', default=1, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--batch-size', default=512, type=int, metavar='N',
+                    help='mini-batch size')
+parser.add_argument('--learning-rate', default=0.06, type=float, metavar='LR',
+                    help='base learning rate')
+
+parser.add_argument('--weight-decay', default=5e-4, type=float, metavar='W',
+                    help='weight decay')
+
+parser.add_argument('--save-dir', default='./checkpoint/', type=Path,
+                    metavar='DIR', help='path to checkpoint directory')
+parser.add_argument('--model-path',default=None)
+
+parser.add_argument('--knn-k', default=200, type=int, metavar='N')
+parser.add_argument('--knn-t', default=0.1, type=int, metavar='N')
+
+parser.add_argument('--in-dim', default=512, type=int, metavar='N')
+parser.add_argument('--h-dim', default=512, type=int, metavar='N')
+parser.add_argument('--out-dim', default=1024, type=int, metavar='N')
+parser.add_argument('--seed', default=42, type=int, metavar='N')
+
+parser.add_argument('--save_dir', default = '/gdrive/MyDrive/simsiam/', type=str)
+parser.add_argument('--exp-name', default = 'resnet_2048_512', type=str)
+
+args=parser.parse_args()
+logger = TensorBoardLogger(os.path.join(gdrive_path,exp_name), name="tensorboard")
+
+class SimSiam(BenchmarkModule):
+
+    def __init__(self, dataloader_kNN, gpus=1, classes=10, knn_k=args.knn_k, knn_t=args.knn_t, in_dim=agrs.in_dim,h_dim=args.h_dim,out_dim=args.out_dim,model_path=None):
+        super().__init__(dataloader_kNN, gpus, classes, knn_k, knn_t)
+        self.resnet = resnet18()   #torchvision.models.resnet18(pretrained=False)
+        self.resnet.fc = nn.Sequential(nn.Linear(in_dim, out_dim, bias=False),  #projection
+                                        nn.BatchNorm1d(out_dim),
+                                        nn.ReLU(inplace=True), 
+                                        nn.Linear(out_dim, out_dim, bias=False),
+                                        nn.BatchNorm1d(out_dim)) 
+
+        # build a 2-layer predictor
+        self.predictor = nn.Sequential(nn.Linear(out_dim, h_dim, bias=False),
+                                        nn.BatchNorm1d(h_dim),
+                                        nn.ReLU(inplace=True), 
+                                        nn.Linear(h_dim, out_dim)) 
+
+        if model_path and os.path.exists(model_path):
+          print('loading the model.....')
+          ckpt=torch.load(model_path)
+          self.resnet.load_state_dict(ckpt['resnet'])
+          self.predictor.load_state_dict(ckpt['predictor'])
+
+
+
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        embedding = self.resnet(x)
+        return embedding
+
+    def D(self, p, z): # negative cosine similarity
+        z = z.detach() # stop gradient
+        p = F.normalize(p, dim=1) # l2-normalize
+        z = F.normalize(z, dim=1) # l2-normalize
+        return -(p*z).sum(dim=1).mean()
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.06, momentum=0.9, weight_decay=5e-4)
+        scheduler = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(optimizer=optimizer, warmup_epochs=10, max_epochs=800, warmup_start_lr=0.0, eta_min=0.0, last_epoch=- 1)
+        return [optimizer], [scheduler]
+
+if __name__ == '__main__':
+
+    log_dir = os.path.join(args.save_dir,args.exp_name,'tensorboard')
+    save_dir = os.path.join(args.save_dir,args.exp_name,'model.ckpt')
+
+    seed_everything(args.seed)
+
+    early_stop_callback = EarlyStopping(monitor='kNN_accuracy', min_delta=0.00, patience=5, verbose=True, mode='min')
+    checkpoint_callback = ModelCheckpoint( monitor='kNN_accuracy', dirpath= os.path.join(gdrive_path,exp_name,'checkpoints'), filename='resnet-epoch-{epoch}-acc-{kNN_accuracy:.2f}', mode='max')
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    knn_train_loader, knn_val_loader=cifar10_loader(args.batch_size)
+    simsiam = SimSiam(knn_train_loader,model_path=args.model_path)
+    trainer = Trainer( gpus=1, max_epochs=args.epochs, min_epochs=1, auto_lr_find=False, auto_scale_batch_size=False,
+                      progress_bar_refresh_rate=1,callbacks=[lr_monitor],logger=logger)
+
+    train_loader, _ = simsiam_cifar10_loader(args.batch_size)
+    
+    trainer.fit(simsiam, train_loader, knn_val_loader)
+    save({'resnet':simsiam.resnet.state_dict(),'predictor':simsiam.predictor.state_dict()},  save_dir)
