d876c64520d6b36cb147ed096cf5706ad2ed37a8
Amro Abbas <37993690+amro-kamal@users.noreply.github.com>
2021-08-13 18:25:51 +0000

    defined knn loader

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ from resnet import resnet18
 import os
 from knn_predictor import BenchmarkModule
 from data import simsiam_cifar10_loader, cifar10_loader
-import numpy as np
-
 
 parser = argparse.ArgumentParser(description='SimSiam Training')
 
@@ -49,9 +47,8 @@ args=parser.parse_args()
 
 class SimSiam(BenchmarkModule):
 
-    def __init__(self, knn_train_loader, gpus=1, classes=10, knn_k=args.knn_k, knn_t=args.knn_t, in_dim=args.in_dim,h_dim=args.h_dim,out_dim=args.out_dim,model_path=None,batch_size=args.batch_size):
+    def __init__(self, gpus=1, classes=10, knn_k=args.knn_k, knn_t=args.knn_t, in_dim=args.in_dim,h_dim=args.h_dim,out_dim=args.out_dim,model_path=None,batch_size=args.batch_size):
         self.save_hyperparameters()
-        self.knn_train_loader = knn_train_loader
         self.batch_size = batch_size
         super().__init__( gpus, classes, knn_k, knn_t)
         self.resnet = resnet18()   #torchvision.models.resnet18(pretrained=False)
@@ -74,7 +71,6 @@ class SimSiam(BenchmarkModule):
           self.predictor.load_state_dict(ckpt['predictor'])
 
 
-
     def forward(self, x):
         # in lightning, forward defines the prediction/inference actions
         embedding = self.resnet(x)
@@ -86,19 +82,16 @@ class SimSiam(BenchmarkModule):
         z = F.normalize(z, dim=1) # l2-normalize
         return -(p*z).sum(dim=1).mean()
 
-
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=0.06, momentum=0.9, weight_decay=5e-4)
         scheduler = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(optimizer=optimizer, warmup_epochs=10, max_epochs=800, warmup_start_lr=0.0, eta_min=0.0, last_epoch=- 1)
         return [optimizer], [scheduler]
-
 
 if __name__ == '__main__':
     seed_everything(args.seed)
 
     log_dir = os.path.join(args.save_dir,args.exp_name,'tensorboard')
     logger = TensorBoardLogger(os.path.join(args.save_dir,args.exp_name), name="tensorboard")
-
 
     #callbacks
     early_stoping_callback = EarlyStopping(monitor='kNN_accuracy', min_delta=0.00, patience=20, verbose=True, mode='min')
@@ -108,9 +101,8 @@ if __name__ == '__main__':
                           every_n_val_epochs=2, save_top_k=1, save_last=True)
     
     #model init
-    knn_train_loader, knn_val_loader = cifar10_loader(args.batch_size)
     if args.ckpt_dir == '':
-        simsiam = SimSiam(knn_train_loader=knn_train_loader, model_path=args.model_path)
+        simsiam = SimSiam(model_path=args.model_path)
         callbacks = [lr_monitor,checkpoint_callback]
         trainer = Trainer( gpus=1, max_epochs=args.epochs, min_epochs=1, auto_lr_find=False, auto_scale_batch_size=False,
                       progress_bar_refresh_rate=1,callbacks=callbacks,logger=logger)
@@ -121,8 +113,9 @@ if __name__ == '__main__':
         # simsiam = SimSiam.load_from_checkpoint(checkpoint_path=args.ckpt_path, strict=False)#, **args.__dict__)
         # callbacks = [ModelCheckpoint(dirpath=args.ckpt_dir)]
         print('____________________________________________________')
-        trainer = Trainer( gpus=1, resume_from_checkpoint=args.ckpt_dir)
+        trainer = Trainer(gpus =1, resume_from_checkpoint=args.ckpt_dir)
 
+    _, knn_val_loader = cifar10_loader(args.batch_size)
     train_loader, _ = simsiam_cifar10_loader(args.batch_size)
     
     trainer.fit(simsiam, train_loader, knn_val_loader)
@@ -131,4 +124,3 @@ if __name__ == '__main__':
     #saving the final model
     save_dir = os.path.join(args.save_dir,args.exp_name,'final_model.ckpt')
     save({'resnet':simsiam.resnet.state_dict(),'predictor':simsiam.predictor.state_dict()},  save_dir)
-
